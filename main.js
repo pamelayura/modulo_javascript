85a70afa244bbc5c36ff5875b3797a82fefab965
Pamela Yura <pamela_yura@hotmail.com>
2023-09-26 19:00:08 -0300

    exercicio mod 7

---
+++ main.js
@@ -0,0 +1,28 @@
+
+const form = document.getElementById('form-validacao');
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let numeroA = document.getElementById('numero-a').value;
+    let numeroB = document.getElementById('numero-b').value;
+    const mensagemSucesso = `Formulário validado com sucesso!`;
+    const mensagemErro = `Não foi possível validar!`;
+
+    if (numeroB>numeroA) {
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        numeroA.value = '';
+        numeroB.value = '';
+
+    } else {
+        const containerMensagemErro = document.querySelector('.error-message');
+        containerMensagemErro.innerHTML = mensagemErro;
+        containerMensagemErro.style.display = 'block';
+    }
+});
+
+
